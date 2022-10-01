5d5f3c31e5b7061149b0431c4a9c1d24cc79e639
Adam Karczewski <akarczewski22@gmail.com>
2022-10-01 11:33:14 +0200

    Main loop added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,37 +9,47 @@ import java.sql.*;
 import java.util.Properties;
 import java.util.Scanner;
 
+/**
+ * Program pozwala wykonać dowolny kod SQL
+ * Pod warunkiem że instrukcje rozdzielone są znakiem nowej lini
+ */
 public class Main {
 
     private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
       String path;
-      System.out.print("Podaj ścieżkę do pliku: ");
-      path = scanner.nextLine();
-      try(Scanner file = new Scanner(Paths.get(path), StandardCharsets.UTF_8);
-          Connection connection = getConnection();
-          Statement statement =connection.createStatement()){
 
-          while (file.hasNextLine()){
-              String line = file.nextLine();
-              // usuwam srednik
-              if(line.endsWith(";"))
-                  line = line.substring(0, line.length()-1);
+      while(true){
+          System.out.print("Podaj ścieżkę do pliku lub exit aby zamknąć program: ");
+          path = scanner.nextLine();
+          if(path.equalsIgnoreCase("exit"))
+              break;
+          try(Scanner file = new Scanner(Paths.get(path), StandardCharsets.UTF_8);
+              Connection connection = getConnection();
+              Statement statement =connection.createStatement()){
 
-              boolean isResult = statement.execute(line);
-              if(isResult){
-                  ResultSet resultSet = statement.getResultSet();
-                  showResultSet(resultSet);
-                  resultSet.close();
+              while (file.hasNextLine()){
+                  String line = file.nextLine();
+                  // usuwam srednik
+                  if(line.endsWith(";"))
+                      line = line.substring(0, line.length()-1);
+
+                  boolean isResult = statement.execute(line);
+                  if(isResult){
+                      ResultSet resultSet = statement.getResultSet();
+                      showResultSet(resultSet);
+                      resultSet.close();
+                  }
               }
-          }
 
-      }catch (IOException e){
-          e.printStackTrace();
-      }catch (SQLException sqlException){
-          for(Throwable t: sqlException)
-              t.printStackTrace();
+          }catch (IOException e){
+              e.printStackTrace();
+          }catch (SQLException sqlException){
+              for(Throwable t: sqlException)
+                  t.printStackTrace();
+          }
       }
+
     }
 
     /**
@@ -66,6 +76,12 @@ public class Main {
         }
     }
 
+    /**
+     * Ustanawia połączenie z bazą danych
+     * @return obiekt klasy Connection
+     * @throws IOException kiedy nie znaleziono pliku database.properties
+     * @throws SQLException kiedy nie ustanowiono połączenia
+     */
 
     private static Connection getConnection() throws IOException, SQLException {
         Properties properties = new Properties();
